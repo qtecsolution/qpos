8d3b0d903e38f725f202386ee668731134276cc6
Azizul Hoque <azizulh8774@gmail.com>
2024-10-30 11:46:21 +0600

    added purchase seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             ProductSeeder::class,
             CustomerSeeder::class,
             SupplierSeeder::class,
+            PurchaseSeeder::class,
         ]);
     }
 }
